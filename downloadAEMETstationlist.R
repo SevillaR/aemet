0a056ffb5df9c15b1cc0c769a970f34f3a99474f
fbalao <fbalao@us.es>
2017-03-04 12:20:26 +0100

    sin key :-)

--- downloadAEMETstationlist.R
+++ downloadAEMETstationlist.R
@@ -1,4 +1,3 @@
-api="eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmYmFsYW9AdXMuZXMiLCJqdGkiOiIxYTM0NTQzMy1hNjQ2LTRhNzMtOTJiNy05N2UwMGY4NzI1YjMiLCJleHAiOjE0OTYzOTgwMjMsImlzcyI6IkFFTUVUIiwiaWF0IjoxNDg4NjIyMDIzLCJ1c2VySWQiOiIxYTM0NTQzMy1hNjQ2LTRhNzMtOTJiNy05N2UwMGY4NzI1YjMiLCJyb2xlIjoiIn0.ARJKETGf_pT_PKCPZjzWzsPDi1Rq37VNS2nRzo72jcc"
 
 downloadAEMETstationlist<-function (api)
   {
@@ -11,6 +10,9 @@ downloadAEMETstationlist<-function (api)
   url <- paste("https://opendata.aemet.es/opendata/api/valores/climatologicos/inventarioestaciones/todasestaciones/")
   md_raw <- curl_fetch_memory(url, h)
   urldatos <- strsplit(rawToChar(md_raw$content), split = "\"")[[1]][10]
+  urlbase <- httr::handle(urldatos)
+  aut <- httr::authenticate(resource[[2]], resource[[3]])
+
   q1 <- httr::GET(handle=urldatos, config=aut, path=paste("api/", resource[[5]], query, sep="") )
   dataQ1 <- httr::content(q1, type="application/json")
   df <- to_dataframe(dataQ1)
