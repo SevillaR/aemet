39f849c5f713c6bcd29a97303326040da146bda9
Pakillo <f.rodriguez.sanc@gmail.com>
2017-04-04 00:24:47 +0200

    added license and aemetpkg help

---
+++ R/aemet-package.R
@@ -0,0 +1,10 @@
+#' R interface to AEMET API
+#'
+#' Download climatic and meteorological data from AEMET directly from R.
+#'
+#' @name aemet-package
+#' @docType package
+#' @aliases aemet
+#' @keywords package
+
+NULL
