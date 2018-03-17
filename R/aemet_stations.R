dff22bb68297a793850b03c6e657e32ffbfbecff
Pakillo <f.rodriguez.sanc@gmail.com>
2018-03-17 13:05:54 +0100

    aemet_stations needs redesign

--- R/aemet_stations.R
+++ R/aemet_stations.R
@@ -15,20 +15,7 @@
 #' stationdata <- as.data.frame(stations)
 #' }
 aemet_stations <- function(apikey) {
-  h <- new_handle()
-  handle_setheaders(h, `Cache-Control` = "no-cache", api_key = apikey)
-  handle_setopt(h, ssl_verifypeer = FALSE)
-  handle_setopt(h, customrequest = "GET")
-  url <- paste("https://opendata.aemet.es/opendata/api/valores/climatologicos/inventarioestaciones/todasestaciones/")
-  md_raw <- curl_fetch_memory(url, h)
-  urldatos <- strsplit(rawToChar(md_raw$content), split = "\"")[[1]][10]
 
-  url2 <- httr::handle(urldatos)
-  set_config(config(ssl_verifypeer = 0L))
-  q1 <- httr::GET(handle = url2)
-  dataQ1 <- httr::content(q1, type = "text/plain", encoding = "ISO-8859-15")
-
-  listQ1 <- fromJSON(dataQ1)
   df <- data.frame(t(sapply(listQ1, function(e) e)))
   station_id <- df[, "indicativo"]
   longString <- as.character(df[, "longitud"])
