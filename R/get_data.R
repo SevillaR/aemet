a05fcadc1abd8c709512c07f7d99c3e61ee41d91
Pakillo <f.rodriguez.sanc@gmail.com>
2018-03-17 13:00:23 +0100

    add generic function to get data from AEMET server

---
+++ R/get_data.R
@@ -0,0 +1,28 @@
+#' Get data from AEMET
+#'
+#' @param apidest character Destination URL. See \url{https://opendata.aemet.es/dist/index.html#!/valores-climatologicos/Inventario_de_estaciones_valores_climatol%C3%B3gicos}.
+#' @param apikey Personal API key (see \url{https://opendata.aemet.es/centrodedescargas/inicio}).
+#'
+#' @return a data.frame
+#' @export
+#' @import httr
+#' @importFrom jsonlite fromJSON
+#'
+#'
+get_data <- function(apidest, apikey) {
+
+  url1 <- paste0(baseurl, apidest)
+
+  path1 <- httr::GET(url1, add_headers(api_key = apikey))
+
+  url.data <- httr::content(path1, as = "parsed")$datos
+  url.metadata <- httr::content(path1, as = "parsed")$metadatos
+
+  #path2 <- GET(url.data, add_headers(api_key = apikey)) # it seems apikey not necessary for this step
+  path2 <- httr::GET(url.data)
+  data.json <- httr::content(path2, as = "text")
+  datos <- jsonlite::fromJSON(data.json)
+
+  datos
+
+}
