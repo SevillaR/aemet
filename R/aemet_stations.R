a7abe77f9b3d90819d8ac5137eb595a6d2da6595
Pakillo <f.rodriguez.sanc@gmail.com>
2018-03-17 16:42:07 +0100

    redesign aemet_stations function

--- R/aemet_stations.R
+++ R/aemet_stations.R
@@ -2,38 +2,22 @@
 #'
 #' @param apikey String with the AEMET API key (see https://opendata.aemet.es/).
 #'
-#' @return A SpatialPointsDataFrame.
+#' @return A data.frame
 #' @export
 #'
-#' @import httr
-
-#'
 #' @examples \dontrun{
-#' stations <- aemet_stations(apikey)  #SpatialPointsDataFrame
-#' stationdata <- as.data.frame(stations)
+#' stations <- aemet_stations(apikey)
 #' }
 aemet_stations <- function(apikey) {
 
   stations <- get_data(apidest = "/api/valores/climatologicos/inventarioestaciones/todasestaciones", apikey)
 
   stations$longitud <- Vectorize(dms2decdegrees)(stations$longitud)
+  stations$latitud <- Vectorize(dms2decdegrees)(stations$latitud)
 
+  df <- stations[, c("indicativo", "indsinop", "nombre", "provincia", "altitud",
+                     "longitud", "latitud")]
 
-  df <- data.frame(t(sapply(listQ1, function(e) e)))
-  station_id <- df[, "indicativo"]
-  longString <- as.character(df[, "longitud"])
-  latString <- as.character(df[, "latitud"])
-
-  x <-
-  points <- SpatialPoints(coords = cbind(-x, y), CRS("+proj=longlat"))
-  colnames(points@coords) <- c("longitude", "latitude")
-  dfout <- data.frame(station_id = df[, "indicativo"],
-                      station = df[, "nombre"],
-                      province = df[, "provincia"],
-                      elevation = as.numeric(df[, "altitud"]))
-
-  finaldf <- SpatialPointsDataFrame(points, dfout)
-
-  return(finaldf)
+  df
 
 }
