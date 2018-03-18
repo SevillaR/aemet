911da13156994e245a03e6c5674dc4c3a3844f28
Pakillo <f.rodriguez.sanc@gmail.com>
2018-03-19 00:07:22 +0100

    use_package_doc

--- R/aemet-package.R
+++ R/aemet-package.R
@@ -1,10 +1,2 @@
-#' R interface to AEMET API
-#'
-#' Download climatic and meteorological data from AEMET directly from R.
-#'
-#' @name aemet-package
-#' @docType package
-#' @aliases aemet
-#' @keywords package
-
-NULL
+#' @keywords internal
+"_PACKAGE"
